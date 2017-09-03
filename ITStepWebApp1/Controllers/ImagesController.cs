590f7079319dbab8637f3a0cd909c422242ad96d
diemon.ukr <diemon.ukr@rambler.ru>
2017-09-03 17:54:02 +0300

    new branch

---
+++ ITStepWebApp1/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using ITStepWebApp1.Data;
+using ITStepWebApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ITStepWebApp1.Controllers
+{
+    // [Authorize]
+    public class ImagesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Upload(HttpPostedFileBase image)
+        {
+            if (image != null)
+            {
+                var imageId = await _store.SaveImage(image.InputStream);
+                return RedirectToAction("Show", new { id = imageId });
+            }
+            return View();
+        }
+
+        public ActionResult Show(string id)
+        {
+            var model = new ShowModel { Uri = _store.UriFor(id) };
+            return View(model);
+        }
+
+        ImageStore _store = new ImageStore();
+    }
+}
---
+++ ITStepWebApp1/Data/ImageStore.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ITStepWebApp1.Data
+{
+    public class ImageStore
+    {
+        public ImageStore()
+        {
+            _client = new CloudBlobClient(_baseUri, new StorageCredentials("itstep2017", "nhtWDa+F01qZHzfMEIo2Vbvls0Jk1sayCeLZGvZB3QvAju8AzOQV2lU4qogSS8tqwCOIuOfDAg+vizJvCMEBsA=="));
+        }
+
+        public async Task<string>SaveImage(Stream stream)
+        {
+            var id = Guid.NewGuid().ToString();
+            var container = _client.GetContainerReference("storagetest");
+            var blob = container.GetBlockBlobReference(id);
+            await blob.UploadFromStreamAsync(stream);
+            return id;
+        }
+
+        public Uri UriFor(string imageId)
+        {
+            var sasPolicy = new SharedAccessBlobPolicy
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessStartTime = DateTime.Now.AddMinutes(-15),
+                SharedAccessExpiryTime = DateTime.Now.AddMinutes(30)
+            };
+            var container = _client.GetContainerReference("storagetest");
+            var blob = container.GetBlockBlobReference(imageId);
+            var sasToken = blob.GetSharedAccessSignature(sasPolicy);
+
+            return new Uri(_baseUri, $"/images/{imageId}{sasToken}");
+        }
+
+        Uri _baseUri = new Uri("https://itstep2017.blob.core.windows.net/");
+        CloudBlobClient _client;
+    }
+}
