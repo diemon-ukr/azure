fe0f0410c9cf3ce484634c2e856d88e1281a47f8
diemon.ukr <diemon.ukr@rambler.ru>
2017-09-03 16:59:40 +0300

    first commit

---
+++ ITStepWebApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITStepWebApp1.Startup))]
+namespace ITStepWebApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
